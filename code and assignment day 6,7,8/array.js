c7ffeee7a61834e8b66641e8b47953c4b180f6a3
tejasrimundrika <mundrikatejasrichinnu@gmail.com>
2025-01-21 11:40:30 +0530

    added code and assignment

---
+++ code and assignment day 6,7,8/array.js
@@ -0,0 +1,120 @@
+// const user1 = "Pavi";
+// const user2 = "sona";
+// const user3 = "akshu";
+// const user4 = "shyam";
+// const user5 = "shabu";
+// console.log(user1);
+// console.log(user2);
+// console.log(user3);
+// console.log(user4);
+// console.log(user5);
+
+// const users=["pavi","teju","akshu","sona","Shyam"];
+// console.log(users[0]);
+// console.log(users[1]);
+// for(i=0;i<users.length;i++){
+//     console.log(users[i]);
+// }
+
+// let x=users.map((user)=>{
+    // console.log(user);
+//     return user;
+// })
+// console.log(x);
+// let x=users.forEach((user)=>{
+    // console.log(user);
+//     return user;
+// })
+// console.log(x);
+
+// const users=["pavi","teju","akshu","sona","Shyam"];
+// users.push("Devi");
+// console.log(users);
+// users.pop();
+// console.log(users);
+
+// users.unshift("Mahesh");
+// console.log(users);
+// users.shift();
+// console.log(users);
+// console.log(users.reverse());
+// const users=["pavi","teju","akshu","sona","Shyam"];
+// let x =users.join("");
+// console.log(x,typeof(x));
+
+// let frontend=["HTML","CSS","JS","REACTJS","ANGULAR","VUE"];
+// let backend =["NODEJS","EXPRESSJS","JAVA","PHP","GOLANG"];
+// let db=["MONGODB","MYSQL","ORACLE","MARIA"];
+// // console.log(frontend);
+// // console.log(backend);
+
+// let fullstack = frontend.concat(backend,db);
+// console.log(fullstack);
+
+// let userDetails ={
+//     name:"Teju",
+//     age:20,
+//     city:"Hyderabad",
+//     address:{
+//         area:"Kammeta",
+//         streetNo:"1",
+//         pinCode:"501503"
+//     }
+// };
+// console.log(userDetails);
+// console.log(userDetails.name);
+// console.log(userDetails.address.area);
+//usinmg the dot notations we can access the keys valuses it is an interview questions
+// let user1={
+//     name:"tej",
+//     city:"huderabad"
+// }
+// let user2={
+//     name:"shab",
+//     city:"dubai"
+// }
+
+// let user3={
+//     name:"aks",
+//     city:"bang"
+// }
+
+// let user4={
+//     name:"shy",
+//     city:"aus"
+// }
+
+// let user5={
+//     name:"son",
+//     city:"ind"
+// }
+// console.log(user1.name);
+// console.log(user2.name);
+// console.log(user3.name);
+// console.log(user4.name);
+// console.log(user5.name);
+
+
+// let userDetails ={
+//     name: "Tejasri",
+//     age:20,
+//     city:"Hyderabad"
+// }
+
+// console.log(userDetails);
+// console.log(userDetails.name);
+// console.log(typeof(userDetails));
+
+// let x=JSON.stringify(userDetails);
+// console.log(x);
+// console.log(x.name);
+// console.log(typeof(x));
+
+
+// let y = JSON.parse(x);
+// console.log(y);
+
+
+
+
+
---
+++ code and assignment day 6,7,8/script.js
@@ -0,0 +1,12 @@
+// const passwordField = document.getElementById('password');
+// const togglePassword = document.getElementById('togglePassword');
+
+// togglePassword.addEventListener('click', () => {
+//     const type = passwordField.type === 'password' ? 'text' : 'password';
+//     passwordField.type = type;
+
+//     // Toggle eye icon
+//     togglePassword.innerHTML = passwordField.type === 'password' 
+//         ? '<i class="fa-solid fa-eye"></i>' 
+//         : '<i class="fa-solid fa-eye-slash"></i>';
+// });
