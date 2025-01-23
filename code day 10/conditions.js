20008a9377144a514f8d3193b304f0feaa8d62b0
tejasrimundrika <mundrikatejasrichinnu@gmail.com>
2025-01-23 10:31:30 +0530

    code is added

---
+++ code day 10/conditions.js
@@ -0,0 +1,120 @@
+// if(true){
+//     console.log("if block is executing");
+// }
+// if(false){
+//     console.log("if block is executing");
+// }
+
+// let age = 18;
+
+// if(age >= 18){
+//     console.log("eligible for voting");
+// }else{
+//     console.log("Not eligible for voting");
+// }
+
+// let isAuthprized = false;
+
+// if(isAuthprized == true){
+//     console.log("Welcome to Payment Page");
+// }else{
+//     console.log("Please login first!!!");
+// }
+
+// let day = prompt("Enter a day number");
+// if(day == 1){
+//     console.log("Sun");
+// }else if(day == 2){
+// console.log("Mon");
+// }
+// else if(day == 3){
+//     console.log("Tue");
+// }
+// else if(day==4){
+//     console.log("wed");
+// }
+// else if(day == 5){
+//     console.log("Thu");
+// }
+// else if(day == 6){
+//     console.log("Fri");
+// }
+// else if(day == 7){
+//     console.log("Sat");
+// }
+// else{
+//     console.log("Invalid day");
+// }
+
+
+// let day = Number(prompt("Enter a day number"));
+// console.log(typeof(day));
+// switch (day) {
+//     case 1:
+//         console.log("Sun");
+//         break;
+//     case 2:
+//         console.log("Mon");
+//         break;
+//         case 3:
+//         console.log("Tue");
+//         break;
+//         case 4:
+//         console.log("Wed");
+//         break;
+//         case 5:
+//         console.log("Thu");
+//         break;
+//         case 6:
+//         console.log("Fri");
+//         break;
+//         case 7:
+//         console.log("Sat");
+//         break;
+//     default:console.log("Invalid day");
+//         break;
+// }
+
+//! find the maximum number among the given values USING ELSE IF CONDITIONS.
+// let a =parseFloat(prompt("Enter the value of a:"));
+// let b =parseFloat(prompt("Enter the value of b:"));
+// let c =parseFloat(prompt("Enter the value of c:"));
+// if(isNaN(a) || isNaN(b) || isNaN(c)){
+//     alert("Invalid numbers");
+// }else if(a===b && b===c){
+//     alert("All the values are equal");
+// }else if(a>b && a>c){
+//     alert("a is the greater number");
+// }else if(b>a && b>c){
+//     alert("b is the greater number");
+// }else if(c>a && c>b){
+//     alert("c is the greater number");
+// }else{
+//     alert("There is no maximum value");
+// }
+
+//USING SWITCH CASE
+// let a =parseFloat(prompt("Enter the value of a:"));
+// let b =parseFloat(prompt("Enter the value of b:"));
+// let c =parseFloat(prompt("Enter the value of c:"));
+// if(isNaN(a) || isNaN(b) || isNaN(c)){
+//     alert("Invalid numbers");
+// }switch(true){
+//     case(a===b && b===c):
+//         alert("All the values are equal");
+//         break;
+//     case(a>b && a>c):
+//         alert("a is the greater number");
+//         break;
+//     case(b>a && b>c):
+//         alert("b is the greater number");
+//         break;
+//     case(c>a && c>b):
+//         alert("c is the greater number");
+//         break;
+//     default:
+//         alert("There is no maximum value");
+//         break;
+// }
+
+
