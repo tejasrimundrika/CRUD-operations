d631fdf503a37ad9138b5e77bd9646e8ab017da9
tejasrimundrika <mundrikatejasrichinnu@gmail.com>
2025-01-30 21:34:09 +0530

    added code

---
+++ code day 15/script1.js
@@ -0,0 +1,5 @@
+let Name ="mru";
+export function greet(){
+    return "Good morning students"+name;
+}
+export default Name;
---
+++ code day 15/script2.js
@@ -0,0 +1,6 @@
+import n from "./script1.js";
+import {greet} from "./script1.js";
+console.log(n);
+// console.log(greet());
+let message=greet();
+console.log(message);
