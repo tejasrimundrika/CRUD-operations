4cc3b66dd4631f1a7ea5f556ca98dcbd9294afdc
tejasrimundrika <mundrikatejasrichinnu@gmail.com>
2025-02-02 15:30:27 +0530

    added code day 16

---
+++ code day 16/src/App.jsx
@@ -0,0 +1,56 @@
+// import React from 'react'
+
+// const App = () => {
+    // let ele = React.createElement("div" ,{id:"demo"},
+    //     React.createElement("h1",null,"Header"),
+    //     React.createElement("p",null,"para")
+    //  )
+//     let x = 1000;
+//     return (
+        // <div>
+        //     <h1>Header</h1>
+        //     <p>Para</p>
+        //     {/* <div>Hello Welcome to React JS</div>
+        //     <p>Lorem ipsum dolor sit.</p> */}
+        // </div>
+        // {ele}
+//         <div>
+//             <h1>React</h1>
+//             <p>Lore ipsum dolor sit amet.</p>
+//             {x}<br/>
+//             {100*2}
+//         </div>
+//     )
+// }
+
+// export default App
+
+// class Parent {
+//     constructor(name,age){
+//         this.name=name;
+//         this.age=age;
+//     }
+// }
+
+// class Child extends Parent{
+//     constructor(name, age ,city){
+//         super(name, age);
+//         this.city=city;
+//     }
+// }
+// import React from "react";
+// class App extends React.Component{
+//     render(){
+//         return(
+//             <h1>Class Component</h1>
+//         )
+//     }
+// };
+// export default App;
+
+function App(){
+    return(
+        <h1>Function component</h1>
+    )
+}
+export default App;
