331b33078c33071128ea6b00269b80ca71469d93
tejasrimundrika <mundrikatejasrichinnu@gmail.com>
2025-02-17 10:02:26 +0530

    Remove deleted files

--- code day 10/dom.js
+++
@@ -1,9 +0,0 @@
-let element =document.getElementById("demo");
-element.innerText = "DOM";
-console.log(element);
-
-let div = document.getElementById("test");
-// div.innerText="<h1>Header</h1>"
-div.innerHTML = "<h1>Header</h1>"
-console.log(div);
-
